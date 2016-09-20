eae86ccafb7a26c2435113d0e88b975ac232bb71
cj222jb <cj222jb@student.lnu.se>
2016-09-20 15:41:21 +0200

    Deleted workshop xml file and small fixes

--- src/View/MainProgram.java
+++ src/View/MainProgram.java
@@ -10,8 +10,6 @@ public class MainProgram {
     static Log log = new Log();
     public static void main(String[]args){
 
-        Boat newBoat = new Boat("Anton",10,10);
-        System.out.println(newBoat.id);
         mem.addMem("Lindmark", 951001, 1);
         System.out.println(log.getMem(1).toString());
     }
