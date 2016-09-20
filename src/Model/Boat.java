952db51b6e6cd88213321aa2f249e167e0c70c36
Anton Lindmark <Anton Lindmark>
2016-09-20 14:16:57 +0200

    test

--- src/Model/Boat.java
+++ src/Model/Boat.java
@@ -4,4 +4,17 @@ package Model;
  * Created by user on 2016-09-20.
  */
 public class Boat {
+
+    public String model;
+    public int id;
+    public int length;
+
+    public Boat(String model,int id,int length){
+        this.model=model;
+        this.id=id;
+        this.length=length;
+    }
+
+
+
 }
--- src/View/MainProgram.java
+++ src/View/MainProgram.java
@@ -1,7 +1,16 @@
 package View;
 
+import Model.Boat;
+
 /**
  * Created by user on 2016-09-20.
  */
 public class MainProgram {
+
+    public static void main(String[]args){
+
+        Boat newBoat = new Boat("Anton",10,10);
+        System.out.println(newBoat.id);
+    }
+
 }
