41fbdd78424c50f169309507647e8562d7419b1c
cj222jb <cj222jb@student.lnu.se>
2016-09-20 14:46:49 +0200

    Added addMem method and log class

---
+++ src/Model/Log.java
@@ -0,0 +1,8 @@
+package Model;
+
+/**
+ * Created by carl on 2016-09-20.
+ */
+public class Log {
+
+}
---
+++ src/View/Console.java
@@ -0,0 +1,13 @@
+package View;
+
+/**
+ * Created by carl on 2016-09-20.
+ */
+public class Console {
+
+
+
+    public void addMem(){
+    }
+
+}
