2279040703212a3954c011628f656489d36d125e
cj222jb <cj222jb@student.lnu.se>
2016-09-20 14:28:26 +0200

    Added Member

--- src/Model/Member.java
+++ src/Model/Member.java
@@ -4,4 +4,20 @@ package Model;
  * Created by user on 2016-09-20.
  */
 public class Member {
+    public String name;
+    public int personalNum;
+    public int memberID;
+
+
+    public Member(String name, int personalNum, int memberID){
+        this.memberID = memberID;
+        this.name = name;
+        this.personalNum = personalNum;
+    }
+
+
+    public void removeMem(int memberID){
+
+    }
+
 }
