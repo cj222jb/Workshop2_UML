54676ff0b35933489069d7c15042b66b15d5dfcc
cj222jb <cj222jb@student.lnu.se>
2016-09-27 13:45:44 +0200

    merge

--- src/View/MainProgram.java
+++ src/View/MainProgram.java
@@ -7,13 +7,13 @@ public class MainProgram {
     static Boat b = new Boat();
 
     public static void main(String[] args){
-<<<<<<< HEAD
+
         mem.addMem("Lindmark", 951001, 0);
 
-=======
+
         mem.addMem("Lindmark", 951001, 45);
         b.addBoat("Motorbåt",1,54);
->>>>>>> 2cf4ee598d063dd803f1bb598c52f5ffa9f8750c
+
         System.out.println(mem.getMem(0).personalNum);
         System.out.println(b.getBoat(0).model);
     }
