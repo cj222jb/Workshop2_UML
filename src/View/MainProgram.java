3006af01016421bb848917ebb338f60e632e4627
Anton Lindmark <Anton Lindmark>
2016-09-28 09:29:03 +0200

    pull

--- src/View/MainProgram.java
+++ src/View/MainProgram.java
@@ -3,11 +3,12 @@ package View;
 public class MainProgram {
     static Console c= new Console();
     public static void main(String[] args){
+
         c.userInterfaceBoat();
 
 
     }
-
+    
 }
 
 
