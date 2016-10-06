9f59c0239cc627ca3236a571894818a744733f34
Anton Lindmark <Anton Lindmark>
2016-10-06 12:46:34 +0200

    pull

--- src/Model/Boat.java
+++ src/Model/Boat.java
@@ -1,21 +1,13 @@
 package Model;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
-/**
- * Created by user on 2016-09-20.
- */
 public class Boat {
-
-
-
-
     static Log log = new Log();
     public String model;
     public int id;
     public int length;
-    HashMap<Integer, Boat> boatList = new HashMap<Integer, Boat>();
-
+    ArrayList<Boat> list = new ArrayList<>();
 
     public Boat(String model,int length,int id){
         this.model=model;
@@ -27,31 +19,15 @@ public class Boat {
     }
 
     public void addBoat(String model, int length, int boatID){
-        Boat newBoat = new Boat(model,length,boatID);
-            boatList.put(boatID, newBoat);
-            log.logboat(newBoat,boatID);
-
+        list.add(new Boat(model,length,boatID));
     }
 
     public Boat getBoat(int boatID){
-        if(containsBoat(boatID))
-            return boatList.get(boatID);
-        else return null;
-
-    }
-
-    public boolean containsBoat(int boatID){
-        if(boatList.get(boatID) != null)
-            return true;
-        else return false;
-
+        return (list.get(boatID));
     }
 
     public void removeBoat(int boatID){
-        if(containsBoat(boatID))
-            boatList.remove(boatID);
-            log.removeBoat(boatID);
-
+        list.remove(boatID);
     }
     public void changeBoat(){
     }
